627545bb83c40448584a999460fcd0a0b8375335
rej42736 <johann.reiter@st.oth-regensburg.de>
2020-09-24 20:39:07 +0200

    Improved formatting of the code

--- src/crypter.cpp
+++ src/crypter.cpp
@@ -4,121 +4,115 @@ using namespace std;
 using namespace png;
 
 crypter::crypter(std::string dataname) : dataname(dataname), size(0), data(nullptr) {
-			//getting size of data
-			ifstream input(dataname, ios::ate);
-			size = input.tellg();
-			input.seekg(0, ios::beg);
-			//size_of_size_in_byte represents the size of the variable size in byte
-			//so that we can allocate fitting memory
-			size_of_size_in_byte = size * 8;
-			size--;
-			data = new char[size];
-			//reads the informations from the file hiddenfile.txt into the array data
-			input.read((char *)data, size);
-			//thats so that the string is ending and doesnt put out random numbers
-			data[size] = '\0';
+
+				//getting size of the file-data
+				ifstream input(dataname, ios::ate);
+				size = input.tellg();
+				input.seekg(0, ios::beg);
+
+				size_of_size_in_byte = size * 8;
+				size--;
+				data = new char[size];
+
+				//reading the data from the file
+				input.read((char *)data, size);
+
 }
 
-void crypter::make_size_binary() { //WORKS
-	size_in_binary = std::bitset<9>(size).to_string();
-}
+//converts the size in binary to store it in the picture
+void crypter::make_size_binary() { size_in_binary = std::bitset<9>(size).to_string(); }
 
-//this is the main hiding function. It hides the data in the picture.
 void crypter::hide_data_in_picture(my_image::image &pic) {
-	//creats binary of size (its_from_size) and binaries of data (itvec);
+
 	make_size_binary();
 	make_binary();
 	hide_information(pic);
+
 }
 
-//this function embeds the information in each pixel. So far, to make it easy, 
-//the function sets each color value of each pixel EQUAL to the bit, so 0 or 1.
-//In the future im gonna change this to the wished setting, thats just for the first 
-//test to make it more easy. Because we're using references, the settings change 
-//with the function for the original pic. Its also iterating up if the binary_iterator
-//for each single bit is gets to big for the string. Is that the case, the 
-//binary_iterator sets to zero and starts iterating from the beginning and the 
-//vector_iterator which iterates through the words increases.
+//stores information in the pixels
 void crypter::embed(my_image::image &pic, int i, int j, int &binary_iterator, int &vector_iterator) {
+
 	my_image::pixel &p = pic(i, j);
+
 	char c = b.bitvec[vector_iterator][binary_iterator];
 	p.r = p.r - p.r % 10 + (c - 48);
 	binary_iterator++;
-	if (binary_iterator >= b.bitvec[vector_iterator].size()) {
-		binary_iterator = 0; vector_iterator++;
-	}
-	if (vector_iterator >= b.bitvec.size()) { 
-		return  ;
-	}
+	if (binary_iterator >= b.bitvec[vector_iterator].size()) { binary_iterator = 0; vector_iterator++; }
+	if (vector_iterator >= b.bitvec.size()) return;
+
 	char d = b.bitvec[vector_iterator][binary_iterator];
 	p.g = p.g - p.g % 10 + (d - 48);
 	binary_iterator++;
-	if (binary_iterator >= b.bitvec[vector_iterator].size()) {
-		binary_iterator = 0; vector_iterator++;
-	}
-	if (vector_iterator >= b.bitvec.size()) { 
-		return  ;
-	}
+	if (binary_iterator >= b.bitvec[vector_iterator].size()) { binary_iterator = 0; vector_iterator++; }
+	if (vector_iterator >= b.bitvec.size()) return;
+
 	char e = b.bitvec[vector_iterator][binary_iterator];
 	p.b = p.b - p.b % 10 + (e - 48);
 	binary_iterator++;
-	if (binary_iterator >= b.bitvec[vector_iterator].size()) {
-		binary_iterator = 0; vector_iterator++;
-	}
-	if (vector_iterator >= b.bitvec.size()) { 
-		return  ;
-	}
+	if (binary_iterator >= b.bitvec[vector_iterator].size()) { binary_iterator = 0; vector_iterator++; }
+	if (vector_iterator >= b.bitvec.size()) return;
+
 }
 
+//stores the size of the file in the first pixels
 void crypter::embed_size(my_image::image &pic, int i, int j, int &binary_iterator) {
+
 	my_image::pixel &p = pic(i, j);
 	char c = size_in_binary[binary_iterator];
 	p.r = p.r - p.r % 10 + c - 48;
+
 	binary_iterator++;
 	char d = size_in_binary[binary_iterator];
 	p.g = p.g - p.g % 10 + d - 48;
 	binary_iterator++;
+
 	char e = size_in_binary[binary_iterator];
 	p.b = p.b - p.b % 10 + e - 48;
 	binary_iterator++;
+
 }
 
-//it hides the information in each pixel by iterating threw it and calling the embed function
-//which really changes each color value of each pixel. 
+//hiding the information in the pixels by iterating through them and calling the embed function
 void crypter::hide_information(my_image::image &pic) {
+
 	int loopiterator = b.size_of_bitvec / 3;
 	if (b.size_of_bitvec % 3 != 0) loopiterator++;
+
 	int j = 0;
 	int binary_iterator = 0;
 	for (int i = 0; i < 3; i++) {
 		embed_size(pic, i, j, binary_iterator);
 		my_image::pixel &x = pic(i, j);
 	}
+
 	binary_iterator = 0;
-	int i2 = 3;
+	int k = 3;
 	int vector_iterator = 0;
 	for (int i = 3; i < (loopiterator + 3); i++) {
-		embed(pic, i2, j, binary_iterator, vector_iterator);
-		if (i2 >= pic.w) { j++; i2 = 0; }
-		i2++;
+		embed(pic, k, j, binary_iterator, vector_iterator);
+		if (k >= pic.w) j++, k = 0;
+		k++;
 	}
-	for (int i = 0; i < loopiterator + 3; i++) {
-		my_image::pixel &x = pic(i, j);
-	}
+
+	for (int i = 0; i < loopiterator + 3; i++) my_image::pixel &x = pic(i, j);
+
 }
 
-void crypter::make_binary() { //WORKS
-	//turns the values of data into binaries and writes the binary in an extra string
+//turns the values of data into binaries and writes the binary in an extra string
+void crypter::make_binary() {
+
 	for (int i = 0; i < size; i++) {
 		b.its = std::bitset<8>(data[i]).to_string();
 		b.bitvec.push_back(b.its);
 	}
-	for (int i = 0; i < size; i++) {
-		reverse(b.bitvec[i].begin(), b.bitvec[i].end());
-	}
+
+	for (int i = 0; i < size; i++) reverse(b.bitvec[i].begin(), b.bitvec[i].end());
+
 	b.its = std::bitset<8>(size).to_string();
 	b.its_from_size = b.its;
 	b.size_of_bitvec = 0;
-	for (int i = 0; i < b.bitvec.size(); i++) 
-		b.size_of_bitvec += b.bitvec[i].size();
+
+	for (int i = 0; i < b.bitvec.size(); i++) b.size_of_bitvec += b.bitvec[i].size();
+
 }
--- src/encrypter.cpp
+++ src/encrypter.cpp
@@ -3,52 +3,52 @@
 using namespace std;
 using namespace png;
 
-//prints the encrypted data
-void encrypter::tell_data() { 
-	std::cout << "Encoded data:\t";
-	for (int i = 0; i < size; i++) std::cout << data[i]; 
-	std::cout << std::endl;
-}
+//extracts the size of the data from the (by i and j) given pixel
+string encrypter::get_size_from_pixel(my_image::image &pic, int i, int j) {
 
-std::string encrypter::get_size_from_pixel(my_image::image &pic, int i, int j) {
 	my_image::pixel &p = pic(i, j);
-	std::string retString = "";
-	retString += std::to_string(p.r % 10);
-	retString += std::to_string(p.g % 10);
-	retString += std::to_string(p.b % 10);
-	return retString;
+	string return_string = "";
+	return_string += to_string(p.r % 10);
+	return_string += to_string(p.g % 10);
+	return_string += to_string(p.b % 10);
+
+	return return_string;
+
 }
 
-void encrypter::get_size(my_image::image &pic) { //WORKS
+//get size of the file and allocate the matching memory to store the data later
+void encrypter::get_size(my_image::image &pic) {
+
 	int j = 0;
-	for (int i = 0; i < 3; i++) {
-		size_binary += get_size_from_pixel(pic, i, j); 
-	}
-	size = std::stoi(size_binary, nullptr, 2);
+	for (int i = 0; i < 3; i++) size_binary += get_size_from_pixel(pic, i, j);
+
+	size = stoi(size_binary, nullptr, 2);
 	size_of_bitvec = size * 8;
 	data = new char[size];
+
 }
 
-//extracts the data from the picture and writes it into the attribute data
+//extracts the data from the picture and writes it into the data-member
 void encrypter::extract_data_from_picture(my_image::image &pic) {
+
 	get_size(pic);
 	get_values_from_pixels(pic);
 	make_binary_to_data();
+
 }
 
-//ATTENTION! THE VECTOR BINVEC HOLDS THE BINARY NUMBERS, BUT WITH BIG ENDIAN, SO THE
-//"WRONG" WAY AROUND! BECAUSE OF THAT I CALCULATED IT THE OTHER WAY.
-void encrypter::make_binary_to_data() { 
-	for (int i = 0; i < size; i++) {
-		bin_vec.push_back("");
-	}
+//turns binary data into real characters. The bin_vec-vector holds the
+//information with big-endian, so we have to read it the other way around.
+void encrypter::make_binary_to_data() {
+
+	for (int i = 0; i < size; i++) bin_vec.push_back("");
+
 	int k = 0;
 	for (int i = 0; i < (size * 8); i++) {
 		if (i % 8 == 0 && i != 0) k++;
 		bin_vec[k] += pixel_binaries[i];
-		//if (i < 8) { bin_vec[0] += pixel_binaries[i]; }
-		//else { bin_vec[1] += pixel_binaries[i]; }
 	}
+
 	for (int i = 0; i < size; i++) {
 		int c = 0;
 		for (int j = 0; j < 8; j++) {
@@ -58,31 +58,46 @@ void encrypter::make_binary_to_data() {
 		}
 		data[i] = c;
 	}
+
 }
 
-std::string encrypter::get_from_pixel(my_image::image &pic, int i, int j, int &sizeiterator) {
-	my_image::pixel &p = pic(i, j); 
-	std::string retString = "";
-	retString += std::to_string(p.r % 10);
+//reads the binary values from the rgb-values of one pixel (given by i and j) and returns the binary
+string encrypter::get_from_pixel(my_image::image &pic, int i, int j, int &sizeiterator) {
+
+	my_image::pixel &p = pic(i, j);
+	string return_string = "";
+
+	return_string += to_string(p.r % 10);
 	sizeiterator++;
-	if (sizeiterator >= size_of_bitvec) return retString;
-	retString += std::to_string(p.g % 10);
+	if (sizeiterator >= size_of_bitvec) return return_string;
+
+	return_string += to_string(p.g % 10);
 	sizeiterator++;
-	if (sizeiterator >= size_of_bitvec) return retString;
-	retString += std::to_string(p.b % 10);
+	if (sizeiterator >= size_of_bitvec) return return_string;
+
+	return_string += to_string(p.b % 10);
 	sizeiterator++;
-	if (sizeiterator >= size_of_bitvec) return retString;
-	return retString;
+	if (sizeiterator >= size_of_bitvec) return return_string;
+
+	return return_string;
+
 }
 
-//writes the binary values of first pixels into a string
-void encrypter::get_values_from_pixels(my_image::image &pic) { 
-	//TESTING:
+//writes the binary values of the first pixels into a string
+void encrypter::get_values_from_pixels(my_image::image &pic) {
+
 	int sizeiterator = 0;
 	int loopiterator = size_of_bitvec / 3 + 3;
 	if (size_of_bitvec % 3 != 0) loopiterator++;
+
 	int j = 0;
-	for (int i = 3; i < loopiterator; i++) {
-		pixel_binaries += get_from_pixel(pic, i, j, sizeiterator);
-	}
+	for (int i = 3; i < loopiterator; i++) pixel_binaries += get_from_pixel(pic, i, j, sizeiterator);
+
+}
+void encrypter::tell_data() {
+
+	cout << "Encoded data:\t";
+	for (int i = 0; i < size; i++) cout << data[i];
+	cout << endl;
+
 }
